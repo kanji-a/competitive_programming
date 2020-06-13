c1c90ffe23a2b614e143c4fc984afd6eaee4da03
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-06-13 20:12:01 +0900

    Cと全く同じだけど一応作成

---
+++ aoj/ALDS1/ALDS1_12_B_test.py
@@ -0,0 +1,31 @@
+from ALDS1_12_B import resolve
+import sys
+from io import StringIO
+import unittest
+
+class TestClass(unittest.TestCase):
+    maxDiff = None
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+    def test_入力例_1(self):
+        input = """5
+0 3 2 3 3 1 1 2
+1 2 0 2 3 4
+2 3 0 3 3 1 4 1
+3 4 2 1 0 1 1 4 4 3
+4 2 2 1 3 3"""
+        output = """0 0
+1 2
+2 2
+3 1
+4 3"""
+        self.assertIO(input, output)
+
+if __name__ == "__main__":
+    unittest.main()
