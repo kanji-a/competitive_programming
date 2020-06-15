5f3c7c12ac7d4ee46cb7c4a3848f38591f7dd1e2
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-06-15 22:37:41 +0900

    15C

---
+++ aoj/ALDS1/ALDS1_15_C_test.py
@@ -0,0 +1,43 @@
+from ALDS1_15_C import resolve
+import sys
+from io import StringIO
+import unittest
+
+class TestClass(unittest.TestCase):
+    maxDiff = None
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+    def test_入力例_1(self):
+        input = """5
+1 2
+3 9
+3 5
+5 9
+6 8"""
+        output = """3"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """3
+1 5
+3 4
+2 5"""
+        output = """1"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """3
+1 2
+2 3
+3 4"""
+        output = """2"""
+        self.assertIO(input, output)
+
+if __name__ == "__main__":
+    unittest.main()
