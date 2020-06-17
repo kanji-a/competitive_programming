8da2e96e947cddc0713326554ae205dbb65ed179
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-06-17 23:47:56 +0900

    1E

---
+++ aoj/DPL/DPL_1_E_test.py
@@ -0,0 +1,27 @@
+from DPL_1_E import resolve
+import sys
+from io import StringIO
+import unittest
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+    def test_入力例_1(self):
+        input = """acac
+acm"""
+        output = """2"""
+        self.assertIO(input, output)
+    def test_入力例_2(self):
+        input = """icpc
+icpc"""
+        output = """0"""
+        self.assertIO(input, output)
+
+if __name__ == "__main__":
+    unittest.main()
