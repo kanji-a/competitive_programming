8ee72aa59690f7df18b41994ec0564995aa7b51d
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-06-19 00:20:53 +0900

    INFは遅いときだけ10**10とかにすればよい importによく使うやつ詰め込んだ

--- aoj/template/a.py
+++ aoj/template/a.py
@@ -1,7 +1,7 @@
-import sys
+import sys, collections, bisect, itertools, heapq, math, copy
 input = lambda: sys.stdin.readline().rstrip() 
 sys.setrecursionlimit(10**7)
-INF = 10**10
+INF = float('inf')
 def I(): return int(input())
 def F(): return float(input())
 def SS(): return input()
