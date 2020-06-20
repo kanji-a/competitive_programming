243c85137c05f802709bcd7d3c2c572fc9c32906
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-06-21 01:19:10 +0900

    15D

--- aoj/ALDS1/ALDS1_15_D.py
+++ aoj/ALDS1/ALDS1_15_D.py
@@ -10,28 +10,43 @@ def LI_(): return [int(x)-1 for x in input().split()]
 def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
-class Node():
-    left = None
-    right = None
-
-class HuffmanTree():
-    root = None
-
 def resolve():
     S = SS()
 
+    # ハフマン木の構成
     cnt = collections.Counter(S)
-    freq = list(cnt.values())
-    ht = HuffmanTree()
-
+    # (頻度, 文字, 左の子, 右の子)
+    freq = [(v, k, None, None) for k, v in cnt.items()]
     heapq.heapify(freq)
     while len(freq) >= 2:
-        print(freq)
         l = heapq.heappop(freq)
         r = heapq.heappop(freq)
-        heapq.heappush(freq, l+r)
-        node = Node()
+        heapq.heappush(freq, (l[0]+r[0], '', l, r))
+    root = heapq.heappop(freq)
 
+    # 符号長の導出
+    code_len = {}
+    for i in cnt.keys():
+        code_len[i] = 1
+
+    def dfs(node, depth):
+        if node[1]:
+            if depth == 0:
+                code_len[node[1]] = 1
+            else:
+                code_len[node[1]] = depth
+        else:
+            dfs(node[2], depth+1)
+            dfs(node[3], depth+1)
+
+    dfs(root, 0)
+
+    # 符号の長さの計算
+    # print(code_len)
+    ans = 0
+    for i in S:
+        ans += code_len[i]
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
