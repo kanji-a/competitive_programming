bda1ee5091ccb66539e3f1a9b4019172e83e2453
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-06-21 20:43:27 +0900

    import増やしてINFをfloat(inf)に

--- atcoder/ABC000/a.py
+++ atcoder/ABC000/a.py
@@ -1,14 +1,14 @@
-import sys
+import sys, collections, bisect, itertools, heapq, math, copy
 input = lambda: sys.stdin.readline().rstrip() 
 sys.setrecursionlimit(10**7)
-INF = 10**20
+INF = float('inf')
 def I(): return int(input())
 def F(): return float(input())
-def S(): return input()
+def SS(): return input()
 def LI(): return [int(x) for x in input().split()]
 def LI_(): return [int(x)-1 for x in input().split()]
 def LF(): return [float(x) for x in input().split()]
-def LS(): return input().split()
+def LSS(): return input().split()
 
 def resolve():
     pass
