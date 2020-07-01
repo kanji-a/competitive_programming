4af722a3e97403d8e9943f53641e478117245416
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-07-02 00:34:47 +0900

    ちょっと考えた

---
+++ atcoder/AGC032/a.py
@@ -0,0 +1,20 @@
+import bisect, collections, copy, heapq, itertools, math, string, sys
+input = lambda: sys.stdin.readline().rstrip() 
+sys.setrecursionlimit(10**7)
+INF = float('inf')
+def I(): return int(input())
+def F(): return float(input())
+def SS(): return input()
+def LI(): return [int(x) for x in input().split()]
+def LI_(): return [int(x)-1 for x in input().split()]
+def LF(): return [float(x) for x in input().split()]
+def LSS(): return input().split()
+
+def resolve():
+    N = I()
+    b = LI()
+    # iはi-1番目以降にしかない
+    # 条件を満たすbの順列を全部試せばOK
+
+if __name__ == '__main__':
+    resolve()
