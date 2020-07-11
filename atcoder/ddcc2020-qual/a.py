9710bc37faf55fb39bf19a6ad8b4bb69fd02e99a
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-07-12 00:58:31 +0900

    a~b

--- atcoder/ddcc2020-qual/a.py
+++ atcoder/ddcc2020-qual/a.py
@@ -1,17 +1,22 @@
-import sys
+import bisect, collections, copy, heapq, itertools, math, string, sys
 input = lambda: sys.stdin.readline().rstrip() 
 sys.setrecursionlimit(10**7)
-INF = 10**20
+INF = float('inf')
 def I(): return int(input())
 def F(): return float(input())
-def S(): return input()
+def SS(): return input()
 def LI(): return [int(x) for x in input().split()]
 def LI_(): return [int(x)-1 for x in input().split()]
 def LF(): return [float(x) for x in input().split()]
-def LS(): return input().split()
+def LSS(): return input().split()
 
 def resolve():
-    pass
+    X, Y = LI_()
+
+    reward = (300000, 200000, 100000, 0)
+
+    ans = reward[min(X, 3)] + reward[min(Y, 3)] + (400000 if X == Y == 0 else 0)
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
--- atcoder/ddcc2020-qual/b.py
+++ atcoder/ddcc2020-qual/b.py
@@ -1,17 +1,28 @@
-import sys
+import bisect, collections, copy, heapq, itertools, math, string, sys
 input = lambda: sys.stdin.readline().rstrip() 
 sys.setrecursionlimit(10**7)
-INF = 10**20
+INF = float('inf')
 def I(): return int(input())
 def F(): return float(input())
-def S(): return input()
+def SS(): return input()
 def LI(): return [int(x) for x in input().split()]
 def LI_(): return [int(x)-1 for x in input().split()]
 def LF(): return [float(x) for x in input().split()]
-def LS(): return input().split()
+def LSS(): return input().split()
 
 def resolve():
-    pass
+    N = I()
+    A = LI()
+
+    acc = [0] * (N + 1)
+    for i in range(N):
+        acc[i+1] = acc[i] + A[i]
+
+    ans = INF
+    for i in acc:
+        ans = min(abs(acc[-1] - 2 * i), ans)
+
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
