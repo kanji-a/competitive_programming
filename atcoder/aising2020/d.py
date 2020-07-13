b79d3d0750dc678784df01437e1d8110b7d131d1
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-07-13 09:41:38 +0900

    d

--- atcoder/aising2020/d.py
+++ atcoder/aising2020/d.py
@@ -15,62 +15,51 @@ def resolve():
     X = SS()
 
     # 一回の操作でnはN以下になる
-    # 操作後に同じ値になることもあるので覚えておく
-    # popcountはそのビット見ればすぐわかる
-    # 初回popcountは2種類しかない
+    # 2回目以降の操作はメモ化再帰
+    # 初回popcountは2種類しかない Xのpopcountから足し引きすればよい
 
-    # x = X
-    # N = 5
-    # X = '10101'
     pc_X = X.count('1')
 
-    next_X_p1 = 0
-    next_X_m1 = 0
-    pc_p1 = pc_X + 1
-    pc_m1 = pc_X - 1
-    for i in range(N):
-        if X[i] == '1':
-            next_X_p1 += pow(2, N - 1 - i, pc_p1)
-            next_X_m1 += pow(2, N - 1 - i, pc_m1)
+    if pc_X == 0:
+        for i in range(N):
+            print(1)
+    elif pc_X == 1:
+        for i in range(N):
+            if i == X.index('1'):
+                print(0)
+            else:
+                # 1が2つある場合、最後の位が1の場合、奇数
+                if i == N - 1 or X.index('1') == N - 1:
+                    print(2)
+                else:
+                    print(1)
+    else:
+        next_X_m1 = 0
+        next_X_p1 = 0
+        pc_m1 = pc_X - 1
+        pc_p1 = pc_X + 1
+        for i in range(N):
+            if X[i] == '1':
+                next_X_m1 += pow(2, N - 1 - i, pc_m1)
+                next_X_p1 += pow(2, N - 1 - i, pc_p1)
 
-    dp = [-1] * N
-    dp[0] = 0
-    def dfs(n):
-        if dp[n] == -1:
-            dp[n] = 1 + dfs(n % bin(n).count('1'))
-        return dp[n]
-    # print(pc_X, pc_m1, pc_p1, next_X_m1, next_X_p1)
-    for i in range(N):
-        next = 0
-        if X[i] == '1':
-            next = next_X_m1 - pow(2, N - 1 - i, pc_m1)
-            next %= pc_m1
-        else:
-            next = next_X_p1 - pow(2, N - 1 - i, pc_p1)
-            next %= pc_p1
-        # for j in range(N):
-        #     if X[j] == '1' and j != i or X[j] == '0' and j == i:
-        #         next += pow(2, N - 1 - j, pc)
-        if int(X) == 1 and i == N - 1:
-            ans = 0
-        else:
+        dp = [-1] * N
+        dp[0] = 0
+
+        def dfs(n):
+            if dp[n] == -1:
+                dp[n] = 1 + dfs(n % bin(n).count('1'))
+            return dp[n]
+
+        for i in range(N):
+            next = 0
+            if X[i] == '0':
+                next = (next_X_p1 + pow(2, N - 1 - i, pc_p1)) % pc_p1
+            else:
+                next = (next_X_m1 - pow(2, N - 1 - i, pc_m1)) % pc_m1
             ans = dfs(next) + 1
-        print(ans)
-    print(dp)
-
-
-
-    # def f(n):
-    #     cnt = 0
-    #     while '1' in n:
-    #         pc = n.count('1')
-    #         n = int(n, 2)
-    #         n %= pc
-    #         n = bin(n)[2:]
-    #         cnt += 1
-    #     print(cnt)
-
-    # print(f(x))
+            print(ans)
+    # print(dp)
 
 if __name__ == '__main__':
     resolve()
