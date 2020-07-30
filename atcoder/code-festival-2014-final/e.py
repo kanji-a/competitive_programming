838b79ef1c50a7d9e70504a3f86d8b56ee8dfad0
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-07-30 16:41:03 +0900

    e

--- atcoder/code-festival-2014-final/e.py
+++ atcoder/code-festival-2014-final/e.py
@@ -11,7 +11,21 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N = I()
+    R = LI()
+
+    diff = [R[i+1] - R[i] for i in range(N - 1) if R[i+1] - R[i] != 0]
+    # print(diff)
+    
+    c = 0
+    for i in range(len(diff) - 1):
+        if diff[i] * diff[i+1] <= -1:
+            c += 1
+
+    if c == 0:
+        print(0)
+    else:
+        print(c + 2)
 
 if __name__ == '__main__':
     resolve()
--- atcoder/code-festival-2014-final/e_test.py
+++ atcoder/code-festival-2014-final/e_test.py
@@ -1,1 +1,55 @@
 from e import resolve
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例1(self):
+        input = """4
+1 2 5 1"""
+        output = """3"""
+        self.assertIO(input, output)
+
+    def test_入力例2(self):
+        input = """5
+1 2 3 4 5"""
+        output = """0"""
+        self.assertIO(input, output)
+
+    def test_入力例3(self):
+        input = """3
+0 1 0"""
+        output = """3"""
+        self.assertIO(input, output)
+
+    def test_入力例4(self):
+        input = """2
+0 1"""
+        output = """0"""
+        self.assertIO(input, output)
+
+    def test_入力例6(self):
+        input = """5
+0 1 0 -1 0"""
+        output = """4"""
+        self.assertIO(input, output)
+
+    def test_入力例7(self):
+        input = """5
+0 1 1 0 1"""
+        output = """4"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
