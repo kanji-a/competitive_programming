a8c3c6e089e52c762bcda5d5e45d92ddecb9959b
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-08-17 20:35:14 +0900

    b

--- atcoder/AGC032/b.py
+++ atcoder/AGC032/b.py
@@ -11,7 +11,37 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N = I()
 
+    # Nが偶数の場合、完全N//2-partiteグラフ
+    # Nが奇数の場合、完全(N-1)//2-partiteグラフに全ての頂点と隣接している頂点Nを加えたもの
+    ans = []
+
+    def f(n):
+        for i in range(1, n // 2 + 1):
+            for j in range(1, n + 1):
+                if j != i and j != n + 1 - i:
+                    if j < i:
+                        ans.append((j, i))
+                    else:
+                        ans.append((i, j))
+                    if j < n + 1 - i:
+                        ans.append((j, n + 1 - i))
+                    else:
+                        ans.append((n + 1 - i, j))
+
+    if N % 2 == 0:
+        f(N)
+    else:
+        if N >= 5:
+            f(N - 1)
+        for i in range(1, N):
+            ans.append((i, N))
+
+    ans = set(ans)
+    print(len(ans))
+    for i, j in ans:
+        print(i, j)
+        
 if __name__ == '__main__':
     resolve()
