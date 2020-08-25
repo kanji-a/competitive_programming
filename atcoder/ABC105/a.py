5743e33d615afc938d0dffc81ec4573f5f98534e
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-08-25 13:31:35 +0900

    a~c

---
+++ atcoder/ABC105/a.py
@@ -0,0 +1,22 @@
+import bisect, collections, copy, heapq, itertools, math, string, sys
+input = lambda: sys.stdin.readline().rstrip() 
+sys.setrecursionlimit(10**7)
+INF = float('inf')
+def I(): return int(input())
+def F(): return float(input())
+def SS(): return input()
+def LI(): return [int(x) for x in input().split()]
+def LI_(): return [int(x)-1 for x in input().split()]
+def LF(): return [float(x) for x in input().split()]
+def LSS(): return input().split()
+
+def resolve():
+    N, K = LI()
+
+    if N % K == 0:
+        print(0)
+    else:
+        print(1)
+
+if __name__ == '__main__':
+    resolve()
---
+++ atcoder/ABC105/c_test.py
@@ -0,0 +1,34 @@
+from c import resolve
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """-9"""
+        output = """1011"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """123456789"""
+        output = """11000101011001101110100010101"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """0"""
+        output = """0"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
