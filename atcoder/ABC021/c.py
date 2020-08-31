8d9e74442f713ee2e0c671d717dee1b5ab5d8286
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-09-01 00:17:11 +0900

    c

--- atcoder/ABC021/c.py
+++ atcoder/ABC021/c.py
@@ -1,7 +1,7 @@
-import sys, collections
+import sys, collections, functools, itertools
 input = lambda: sys.stdin.readline().rstrip() 
 sys.setrecursionlimit(10**7)
-INF = 10**20
+INF = float('inf')
 MOD = 1000000007
 def I(): return int(input())
 def F(): return float(input())
@@ -15,39 +15,40 @@ def resolve():
     N = I()
     a, b = LI_()
     M = I()
-    con = [[False]*N for _ in range(N)]
-    for i in range(M):
-        xy = LI_()
-        con[xy[0]][xy[1]] = True
-        con[xy[1]][xy[0]] = True
+    G = collections.defaultdict(list)
+    for _ in range(M):
+        x, y = LI_()
+        G[x].append(y)
+        G[y].append(x)
+    # print(G)
 
-    ans = [0]
-    deq = collections.deque()
-    deq.append(a)
-    dist = [INF]*N
+    deq = collections.deque([a])
+    dist = [INF] * N
     dist[a] = 0
+ 
+    # 頂点毎の最短距離を探索する
+    while deq:
+        c = deq.popleft()
+        for n in G[c]:
+            if dist[n] == INF:
+                deq.append(n) 
+                dist[n] = dist[c] + 1
 
-    while len(deq)>0:
-        city_c = deq.popleft()
-        for city_n in range(N):
-            if con[city_c][city_n] and dist[city_n]>=INF:
-                deq.append(city_n) 
-                dist[city_n] = dist[city_c]+1
-    
-    dist_city = [[] for _ in range(100)]
-    for i, e in enumerate(dist):
-        dist_city[e].append(i)
-
-    def dfs(city_c, depth):
-        if city_c==b:
-            ans[0] += 1
+    # cからbへの経路数
+    @functools.lru_cache
+    def dfs(c):
+        if c == b:
+            return 1
         else:
-            for city_n in dist_city[depth+1]:
-                if con[city_c][city_n]:
-                    dfs(city_n, depth+1)
+            ret = 0
+            for n in G[c]:
+                if dist[n] == dist[c] + 1:
+                    ret += dfs(n)
+                    ret %= MOD
+            return ret
 
-    dfs(a, 0)
-    print(ans[0])
+    ans = dfs(a)
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
