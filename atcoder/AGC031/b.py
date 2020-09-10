9a17fbea8bb460c16d362a2e7347781831c68805
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-09-11 00:54:23 +0900

    途中

--- atcoder/AGC031/b.py
+++ atcoder/AGC031/b.py
@@ -2,6 +2,7 @@ import bisect, collections, copy, heapq, itertools, math, string, sys
 input = lambda: sys.stdin.readline().rstrip() 
 sys.setrecursionlimit(10**7)
 INF = float('inf')
+MOD = 10 ** 9 + 7
 def I(): return int(input())
 def F(): return float(input())
 def SS(): return input()
@@ -11,7 +12,15 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N = I()
+    C = [I() for _ in range(N)]
+
+    # 1回以内の場合にしか答えていない
+    cc = [k for k, _ in itertools.groupby(C)]
+    print(cc)
+    cnt = collections.Counter(cc)
+    ans = (sum([i * (i - 1) // 2 % MOD for i in cnt.values()]) + 1) % MOD
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
--- atcoder/AGC031/b_test.py
+++ atcoder/AGC031/b_test.py
@@ -1,1 +1,52 @@
 from b import resolve
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """5
+1
+2
+1
+2
+2"""
+        output = """3"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """6
+4
+2
+5
+4
+2
+4"""
+        output = """5"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """7
+1
+3
+1
+2
+3
+3
+2"""
+        output = """5"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
