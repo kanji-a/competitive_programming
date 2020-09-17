40732182c088b4c6b0a146484a14047541b19841
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-09-17 14:36:29 +0900

    d

--- atcoder/ABC038/d.py
+++ atcoder/ABC038/d.py
@@ -10,32 +10,23 @@ def LI_(): return [int(x)-1 for x in input().split()]
 def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
-def binsearch_left(a, x):
-    ng = -1
-    ok = len(a)
-    while abs(ok-ng)>1:
-        m = (ng+ok)//2
-        if x[0] < a[m][0] and x[1] < a[m][1]:
-            ok = m
-        else:
-            ng = m
-    return ok
-
 def lis(a):
-    dp = [[INF, INF]]*len(a)
+    dp = [INF]*len(a)
     for i in a:
-        dp[binsearch_left(dp, i)] = i
-    return binsearch_left(dp, [INF, INF])
+        dp[bisect.bisect_left(dp, i)] = i
+    return bisect.bisect_left(dp, INF)
 
 def resolve():
     N = I()
     hw = [LI() for _ in range(N)]
 
-    # LISでやろうとしたができず
-    # 列の中で2つ組の大小関係未定義のペアが存在するから?
-    hw.sort()
-    print(hw)
-    ans = lis(hw)
+    # hでソートしてwのLISをとる
+    # 同じhのwを複数取らないよう、wを降順ソートしておく
+    hw.sort(key=lambda x: x[1], reverse=True)
+    hw.sort(key=lambda x: x[0])
+    w = [i[1] for i in hw]
+
+    ans = lis(w)
     print(ans)
 
 if __name__ == '__main__':
