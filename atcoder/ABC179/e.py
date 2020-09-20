024fb3d9b8160cd5509baf49d15a216bed91add7
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-09-20 23:48:20 +0900

    e

--- atcoder/ABC179/e.py
+++ atcoder/ABC179/e.py
@@ -11,7 +11,35 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N, X, M = LI()
+
+    A = []
+    seen = set()
+    tmp = X
+    head_idx = -1
+    while True:
+        A.append(tmp)
+        seen.add(tmp)
+        tmp = pow(tmp, 2, M)
+        if tmp in seen:
+            head_idx = A.index(tmp)
+            break
+    # print(A, head_idx)
+
+    cnt = [0] * len(A)
+    for i in range(len(A)):
+        if i < head_idx:
+            if i <= N:
+                cnt[i] = 1
+        else:
+            l = len(A) - head_idx
+            cnt[i] = (N - head_idx) // l
+    for i in range(head_idx, head_idx + (N - head_idx) % l):
+        cnt[i] += 1
+    # print(cnt)
+
+    ans = sum([a * c for a, c in zip(A, cnt)])
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
