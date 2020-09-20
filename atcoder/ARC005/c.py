a2d60a09a64b98961da56cc1e9c9d7a6fd964e74
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-09-21 00:15:55 +0900

    c

--- atcoder/ARC005/c.py
+++ atcoder/ARC005/c.py
@@ -11,7 +11,43 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    H, W = LI()
+    c = [SS() for _ in range(H)]
+
+    d = ((1, 0), (0, 1), (-1, 0), (0, -1))
+    s = ()
+    g = ()
+    for i in range(H):
+        for j in range(W):
+            if c[i][j] == 's':
+                s = (i, j)
+            if c[i][j] == 'g':
+                g = (i, j)
+
+    que = collections.deque([s])
+    dist = [[INF] * W for _ in range(H)]
+    dist[s[0]][s[1]] = 0
+    while que:
+        cy, cx = que.popleft()
+        for dy, dx in d:
+            ny = cy + dy
+            nx = cx + dx
+            if 0 <= ny < H and 0 <= nx < W:
+                if c[ny][nx] == '#':
+                    if dist[ny][nx] > dist[cy][cx] + 1:
+                        dist[ny][nx] = dist[cy][cx] + 1
+                        que.append((ny, nx))
+                else:
+                    if dist[ny][nx] > dist[cy][cx]:
+                        dist[ny][nx] = dist[cy][cx]
+                        que.appendleft((ny, nx))
+                    
+    # for i in dist:
+    #     print(i)
+    if dist[g[0]][g[1]] <= 2:
+        print('YES')
+    else:
+        print('NO')
 
 if __name__ == '__main__':
     resolve()
