1b84240143083b7b9b08b15802ba67bc5ff917d0
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-09-20 23:48:29 +0900

    a~c

---
+++ atcoder/ABC005/c.py
@@ -0,0 +1,33 @@
+import bisect, collections, copy, heapq, itertools, math, string, sys
+input = lambda: sys.stdin.readline().rstrip() 
+sys.setrecursionlimit(10**7)
+INF = float('inf')
+def I(): return int(input())
+def F(): return float(input())
+def SS(): return input()
+def LI(): return [int(x) for x in input().split()]
+def LI_(): return [int(x)-1 for x in input().split()]
+def LF(): return [float(x) for x in input().split()]
+def LSS(): return input().split()
+
+def resolve():
+    T = I()
+    N = I()
+    A = LI()
+    M = I()
+    B = LI()
+
+    ans = 'yes'
+    j = 0
+    for i in B:
+        while j < N and i - T > A[j]:
+            j += 1
+        if j >= N or A[j] > i:
+            ans = 'no'
+            break
+        j += 1
+
+    print(ans)
+
+if __name__ == '__main__':
+    resolve()
