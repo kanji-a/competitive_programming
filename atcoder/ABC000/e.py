4a683a8eec5c2a31fc738d9c7718d0ea265cb487
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-09-27 12:27:55 +0900

    テンプレにMODの値追加

--- atcoder/ABC000/e.py
+++ atcoder/ABC000/e.py
@@ -2,6 +2,7 @@ import bisect, collections, copy, heapq, itertools, math, string, sys
 input = lambda: sys.stdin.readline().rstrip() 
 sys.setrecursionlimit(10**7)
 INF = float('inf')
+MOD = 10**9+7
 def I(): return int(input())
 def F(): return float(input())
 def SS(): return input()
