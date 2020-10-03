c73666e91542271f04dbdca8d14a5fa4a5f3a585
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-10-04 02:03:15 +0900

    d

--- atcoder/ABC015/d.py
+++ atcoder/ABC015/d.py
@@ -11,7 +11,23 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    W = I()
+    N, K = LI()
+    AB = [LI() for _ in range(N)]
+
+    # dp[i][j]: 合計幅i以内の個数jの最大合計重要度
+    dp = [[0] * (K + 1) for _ in range(W + 1)]
+    for i in range(N):
+        A, B = AB[i]
+        for j in range(W, 0, -1):
+            for k in range(1, K + 1):
+                if j - A >= 0:
+                    dp[j][k] = max(dp[j][k], dp[j-A][k-1] + B)
+    # for i in dp:
+    #     print(i)
+
+    ans = max([max(i) for i in dp])
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
