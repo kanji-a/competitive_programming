b9377494bb58a894af7ac606033600468e3d126b
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-10-14 00:50:05 +0900

    e

--- atcoder/tdpc/e.py
+++ atcoder/tdpc/e.py
@@ -1,4 +1,4 @@
-import bisect, collections, copy, heapq, itertools, math, string, sys
+import bisect, collections, copy, heapq, functools, itertools, math, string, sys
 input = lambda: sys.stdin.readline().rstrip() 
 sys.setrecursionlimit(10**7)
 INF = float('inf')
@@ -12,7 +12,28 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    D = I()
+    N = SS()
 
+    len_N = len(N)
+
+    # dp[i][j][k]: [0, i)桁目まで見た時, j:未満フラグ, k: modD
+    dp = [[[0] * D for _ in range(2)] for _ in range(len_N + 1)]
+    dp[0][0][0] = 1
+    for i in range(len_N):
+        N_i = int(N[i])
+        for j in range(D):
+            for k in range(10):
+                dp[i+1][1][(j+k)%D] += dp[i][1][j]
+                dp[i+1][1][(j+k)%D] %= MOD
+            for k in range(N_i):
+                dp[i+1][1][(j+k)%D] += dp[i][0][j]
+                dp[i+1][1][(j+k)%D] %= MOD
+            dp[i+1][0][(j+N_i)%D] += dp[i][0][j]
+            dp[i+1][0][(j+N_i)%D] %= MOD
+    # print(dp)
+
+    print((dp[-1][0][0] + dp[-1][1][0] - 1) % MOD)
+    
 if __name__ == '__main__':
     resolve()
--- atcoder/tdpc/e_test.py
+++ atcoder/tdpc/e_test.py
@@ -1,1 +1,31 @@
 from e import resolve
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_Sample_Input_1(self):
+        input = """3
+100"""
+        output = """33"""
+        self.assertIO(input, output)
+
+    def test_Sample_Input_2(self):
+        input = """7
+123456789012345678901234567890"""
+        output = """468357804"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
