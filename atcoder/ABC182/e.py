fbeb198381e507e7c28a23ae715c2430e8cdd85b
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-11-12 00:49:06 +0900

    e

--- atcoder/ABC182/e.py
+++ atcoder/ABC182/e.py
@@ -13,27 +13,60 @@ def LSS(): return input().split()
 
 def resolve():
     H, W, N, M = LI()
-    AB = [LI_() for _ in range(N)]
     S = [['.'] * W for _ in range(H)]
+    for _ in range(N):
+        A, B = LI_()
+        S[A][B] = '*'
     for _ in range(M):
         C, D = LI_()
         S[C][D] = '#'
 
-    for i in S:
-        print(i)
+    # for i in S:
+    #     print(i)
 
-    left = [[0] * W for _ in range(H)]
+    # 横方向の光
+    light_h = copy.deepcopy(S)
     for i in range(H):
-        cnt = 0
         for j in range(W):
-            if S[i][j] == '.':
-                cnt += 1
-                left[i][j] = cnt
-            else:
-                cnt = 0
+            # 電球を見つけたら左右に光を伸ばしていく
+            if S[i][j] == '*':
+                # 左方向
+                k = j - 1
+                while k >= 0 and light_h[i][k] == '.':
+                    light_h[i][k] = '*'
+                    k -= 1 
+                # 右方向
+                k = j + 1
+                while k < W and light_h[i][k] == '.':
+                    light_h[i][k] = '*'
+                    k += 1 
+    # for i in light_h:
+    #     print(i)
 
-    for i in left:
-        print(i)
+    # 縦方向の光
+    light_v = copy.deepcopy(S)
+    for i in range(W):
+        for j in range(H):
+            # 電球を見つけたら左右に光を伸ばしていく
+            if S[j][i] == '*':
+                # 上方向
+                k = j - 1
+                while k >= 0 and light_v[k][i] == '.':
+                    light_v[k][i] = '*'
+                    k -= 1 
+                # 下方向
+                k = j + 1
+                while k < H and light_v[k][i] == '.':
+                    light_v[k][i] = '*'
+                    k += 1 
+    # for i in light_v:
+    #     print(i)
+
+    ans = 0
+    for i, j in itertools.product(range(H), range(W)):
+        if light_h[i][j] == '*' or light_v[i][j] == '*':
+            ans += 1
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
