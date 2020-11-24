717cb2ee406efbb49f586d44fbd035ee6ec88c0f
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-11-24 21:16:23 +0900

    after_contestケースも通るようにした

--- atcoder/ABC184/c.py
+++ atcoder/ABC184/c.py
@@ -11,6 +11,11 @@ def LI_(): return [int(x)-1 for x in input().split()]
 def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
+# 一手でいけるか
+def f(r1, c1, r2, c2):
+    if r1 + c1 == r2 + c2 or r1 - c1 == r2 - c2 or abs(r1 - r2) + abs(c1 - c2) <= 3:
+        return True
+
 def resolve():
     r1, c1 = LI()
     r2, c2 = LI()
@@ -18,13 +23,20 @@ def resolve():
     ans = -1
     if r1 == r2 and c1 == c2:
         ans = 0
-    elif r1 + c1 == r2 + c2 or r1 - c1 == r2 - c2 or abs(r1 - r2) + abs(c1 - c2) <= 3:
+    elif f(r1, c1, r2, c2):
         ans = 1
-    # 斜め移動2回か、斜め移動後に四角に入るか
-    elif (r1 + c1) % 2 == (r2 + c2) % 2 or abs((c1 - r1) - abs(c2 - r2)) <= 3 or abs((c1 + r1) - (c2 + r2)) <= 3:
-        ans = 2
     else:
-        ans = 3
+        # ◇移動の後にX移動するパターン
+        is_dia_diag = False
+        for i in range(-3, 4):
+            for j in range(-3 + abs(i), 4 - abs(i)):
+                if f(r1 + i, c1 + j, r2, c2):
+                    is_dia_diag = True
+        # X移動2回(パリティ同じマスへ行く)も含める
+        if is_dia_diag or (r1 + c1) % 2 == (r2 + c2) % 2:
+            ans = 2
+        else:
+            ans = 3
     
     print(ans)
 
