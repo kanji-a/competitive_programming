3ef52a8631c0100687127e306917ac8fc69ea853
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-12-01 23:17:53 +0900

    d

--- atcoder/ABC036/d.py
+++ atcoder/ABC036/d.py
@@ -1,7 +1,8 @@
-import bisect, collections, copy, heapq, itertools, math, string, sys
+import bisect, collections, copy, functools, heapq, itertools, math, string, sys
 input = lambda: sys.stdin.readline().rstrip() 
 sys.setrecursionlimit(10**7)
 INF = float('inf')
+MOD = 10 ** 9 + 7
 def I(): return int(input())
 def F(): return float(input())
 def SS(): return input()
@@ -11,7 +12,26 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N = I()
+    G = collections.defaultdict(list)
+    for _ in range(N - 1):
+        a, b = LI_()
+        G[a].append(b)
+        G[b].append(a)
+
+    dp = [[1] * 2 for _ in range(N)]
+    def f(c, p):
+        for n in G[c]:
+            if n != p:
+                f(n, c)
+                dp[c][0] *= (dp[n][0] + dp[n][1])
+                dp[c][0] %= MOD
+                dp[c][1] *= dp[n][0]
+                dp[c][1] %= MOD
+
+    f(0, -1)
+    ans = dp[0][0] + dp[0][1]
+    print(ans % MOD)
 
 if __name__ == '__main__':
     resolve()
--- atcoder/ABC036/d_test.py
+++ atcoder/ABC036/d_test.py
@@ -1,1 +1,42 @@
 from d import resolve
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例1(self):
+        input = """5
+2 5
+1 5
+2 4
+3 2"""
+        output = """14"""
+        self.assertIO(input, output)
+
+    def test_入力例2(self):
+        input = """10
+7 9
+8 1
+9 6
+10 8
+8 6
+10 3
+5 8
+4 8
+2 5"""
+        output = """192"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
