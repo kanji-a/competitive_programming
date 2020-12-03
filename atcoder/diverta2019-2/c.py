ce294b8e7c2c13528487ffcf46c25dcd775031bb
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-12-03 14:43:37 +0900

    c

--- atcoder/diverta2019-2/c.py
+++ atcoder/diverta2019-2/c.py
@@ -14,45 +14,67 @@ def resolve():
     N = I()
     A = LI()
 
-    # 0をA_nにすると1ケースWAが減る謎
-    A_p = [i for i in A if i >= 0]
+    # 残った値のMaxはsum(B) - sum(C)
+    # BもCもAの部分列でサイズ1以上
+    # BとCはAを2つに分割したもの
+    B = []
+    C = []
+    A_p = []
+    A_n = []
+    A_z = []
+    for i in A:
+        if i == 0:
+            A_z.append(i)
+        elif i > 0:
+            A_p.append(i)
+        else:
+            A_n.append(i)
     A_p.sort()
-    A_n = [i for i in A if i < 0]
-    A_n.sort()
+    A_n.sort(reverse=True)
 
-    if A_p and A_n:
-        # 正の数を1個以外負の数から引く
-        # 残した正の数から負の数を全部引く
-        print(sum(A_p) - sum(A_n))
+    if A_p:
+        if A_n:
+            B = A_p + A_z
+            C = A_n
+        else:
+            if A_z:
+                B = A_p
+                C = A_z
+            else:
+                B = A_p[1:]
+                C = A_p[:1]
+    else:
+        if A_n:
+            if A_z:
+                B = A_z
+                C = A_n
+            else:
+                B = A_n[:1]
+                C = A_n[1:]
+        else:
+            B = A_z[:1]
+            C = A_z[1:]
 
-        tmp = A_n[0]
-        for i in range(1, len(A_p)):
-            print(tmp, A_p[i])
-            tmp -= A_p[i]
-        print(A_p[0], tmp)
-        tmp = A_p[0] - tmp
-        for i in range(1, len(A_n)):
-            print(tmp, A_n[i])
-            tmp -= A_n[i]
-
-    elif A_p and not A_n:
-        # 正の数しかない場合、最小値から他の数字を1個残して引いたものを、残った数字から引く
-        print(sum(A_p[1:]) - A_p[0])
-
-        tmp = A_p[0]
-        for i in range(1, len(A_p) - 1):
-            print(tmp, A_p[i])
-            tmp -= A_p[i]
-        print(A[-1], tmp)
-
-    elif not A_p and A_n:
-        # 負の数しかない場合、最大値から他の数字を全部引く
-        print(A_n[-1] - sum(A_n[:-1]))
-
-        tmp = A_n[-1]
-        for i in range(len(A_n) - 1):
-            print(tmp, A_n[i])
-            tmp -= A_n[i]
+    M = sum(B) - sum(C)
+    print(M)
+    if len(B) == 1:
+        # B[0]からCを引くだけ
+        num = B[0]
+        for i in C:
+            print(num, i)
+            num -= i
+    else:
+        # Bを足すために、一旦C[0]からB[1:]を全部引いて、それをB[0]から引く
+        # その後はC[1:]を引くだけ
+        num = C[0]
+        for i in B[1:]:
+            print(num, i)
+            num -= i
+        print(B[0], num)
+        num = B[0] - num
+        for i in C[1:]:
+            print(num, i)
+            num -= i
 
 if __name__ == '__main__':
     resolve()
