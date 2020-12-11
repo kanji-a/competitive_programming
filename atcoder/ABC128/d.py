37c5329252a2552f7efb4ebab255d4b1efcc2588
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2020-12-11 22:50:59 +0900

    d

--- atcoder/ABC128/d.py
+++ atcoder/ABC128/d.py
@@ -1,8 +1,44 @@
-import sys
+import bisect, collections, copy, heapq, itertools, math, string, sys
 input = lambda: sys.stdin.readline().rstrip() 
+sys.setrecursionlimit(10**7)
+INF = float('inf')
+MOD = 10**9+7
+def I(): return int(input())
+def F(): return float(input())
+def SS(): return input()
+def LI(): return [int(x) for x in input().split()]
+def LI_(): return [int(x)-1 for x in input().split()]
+def LF(): return [float(x) for x in input().split()]
+def LSS(): return input().split()
 
 def resolve():
-    pass
+    N, K = LI()
+    V = LI()
+
+    # 先に取り出して、後に戻すという順番でよい
+    # 取り出す深さを決めて、残った手数でいらないのを戻す
+    ans = 0
+    # l, r: 左、右から取る個数
+    for l, r in itertools.product(range(min(N, K) + 1), repeat=2):
+        if l + r <= min(N, K):
+            score = 0
+            minus = []
+            for i in range(l):
+                if V[i] >= 0:
+                    score += V[i]
+                else:
+                    minus.append(V[i])
+            for i in range(r):
+                if V[N-1-i] >= 0:
+                    score += V[N-1-i]
+                else:
+                    minus.append(V[N-1-i])
+            minus.sort()
+            # 残った手数で負の価値の石を戻す
+            score += sum(minus[min(K-l-r, len(minus)):])
+            ans = max(score, ans)
+
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
--- atcoder/ABC128/d_test.py
+++ atcoder/ABC128/d_test.py
@@ -1,1 +1,37 @@
 from d import resolve
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """6 4
+-10 8 2 1 2 6"""
+        output = """14"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """6 4
+-6 -100 50 -2 -5 -3"""
+        output = """44"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """6 3
+-6 -100 50 -2 -5 -3"""
+        output = """0"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
