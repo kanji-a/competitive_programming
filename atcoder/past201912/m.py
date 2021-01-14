f725ce6276753f9033f14f7cfc3d2ab7d7ccfd78
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2021-01-14 22:29:08 +0900

    m

--- atcoder/past201912/m.py
+++ atcoder/past201912/m.py
@@ -12,7 +12,30 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N, M = LI()
+    AB = [LI() for _ in range(N)]
+    CD = [LI() for _ in range(M)]
+
+    # 決め打ち二分探索
+    # ΣB/ΣA≧X ⇔ Σ(B-XA)≧0
+
+    def is_ok(X):
+        AB_ = [b - X * a for a, b in AB]
+        CD_ = [d - X * c for c, d in CD]
+        sorted_AB = sorted(AB_, reverse=True)
+        res = sum(sorted_AB[:5]) >= 0 or sum(sorted_AB[:4]) + max(CD_) >= 0
+        return res
+
+    ng = 10 ** 9
+    ok = 0
+    while abs(ok - ng) > 10e-7:
+        m = (ng + ok) / 2
+        if is_ok(m):
+            ok = m
+        else:
+            ng = m
+
+    print(m)
 
 if __name__ == '__main__':
     resolve()
--- atcoder/past201912/m_test.py
+++ atcoder/past201912/m_test.py
@@ -1,1 +1,45 @@
 from m import resolve
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """6 2
+10 30
+20 60
+10 10
+30 100
+50 140
+40 120
+10 3
+30 1"""
+        output = """3.0000000000000"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """6 2
+1 20
+1 3
+32 100
+1 1
+1 2
+2 5
+10 100
+96 874"""
+        output = """9.0000000000000"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
