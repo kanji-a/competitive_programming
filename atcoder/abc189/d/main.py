1d1b7fb123a9e2d51054ced3c5798e8c10350686
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2021-02-06 23:51:00 +0900

    d

--- atcoder/abc189/d/main.py
+++ atcoder/abc189/d/main.py
@@ -13,7 +13,23 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N = I()
+    S = [SS() for _ in range(N)]
+
+    # dp[i][j]: y_iがjになるものの個数
+    dp = [[0] * 2 for _ in range(N + 1)]
+    dp[0][0] = 1
+    dp[0][1] = 1
+    for i in range(N):
+        if S[i] == 'AND':
+            dp[i+1][1] = dp[i][1]
+            dp[i+1][0] = dp[i][0] * 2 + dp[i][1]
+        else:
+            dp[i+1][1] = dp[i][0] + dp[i][1] * 2
+            dp[i+1][0] = dp[i][0]
+    # print(dp)
+
+    print(dp[-1][1])
 
 if __name__ == '__main__':
     resolve()
