3c08681ded3f851f76752f2d2806d8097992ee21
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2021-02-07 00:09:48 +0900

    a~d

---
+++ atcoder/past202012-open/a/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import bisect, collections, copy, heapq, itertools, math, operator, string, sys, typing
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10 ** 7)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+def I(): return int(input())
+def F(): return float(input())
+def SS(): return input()
+def LI(): return [int(x) for x in input().split()]
+def LI_(): return [int(x)-1 for x in input().split()]
+def LF(): return [float(x) for x in input().split()]
+def LSS(): return input().split()
+
+def resolve():
+    S = SS()
+    if 'ooo' in S:
+        print('o')
+    elif 'xxx' in S:
+        print('x')
+    else:
+        print('draw')
+
+if __name__ == '__main__':
+    resolve()
+
---
+++ atcoder/past202012-open/l/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import bisect, collections, copy, heapq, itertools, math, operator, string, sys, typing
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10 ** 7)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+def I(): return int(input())
+def F(): return float(input())
+def SS(): return input()
+def LI(): return [int(x) for x in input().split()]
+def LI_(): return [int(x)-1 for x in input().split()]
+def LF(): return [float(x) for x in input().split()]
+def LSS(): return input().split()
+
+def resolve():
+    pass
+
+if __name__ == '__main__':
+    resolve()
+
