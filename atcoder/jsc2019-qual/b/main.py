7d6eccb268b1e067543ed9a783150c3adc4ab289
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2021-03-08 01:11:14 +0900

    b

--- atcoder/jsc2019-qual/b/main.py
+++ atcoder/jsc2019-qual/b/main.py
@@ -13,7 +13,29 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N, K = LI()
+    A = LI()
+
+    # A内の転倒数
+    inv_num_in = 0
+    for i in range(N):
+        for j in range(i + 1, N):
+            if A[i] > A[j]:
+                inv_num_in += 1
+                inv_num_in %= MOD
+    # print(inv_num_in)
+
+    # 異なるA間の転倒数
+    inv_num_out = 0
+    for i in range(N):
+        for j in range(N):
+            if A[i] > A[j]:
+                inv_num_out += 1
+                inv_num_out %= MOD
+    # print(inv_num_out)
+
+    ans = (inv_num_in * K % MOD + inv_num_out * K * (K - 1) // 2 % MOD) % MOD
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
