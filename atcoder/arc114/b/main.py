4ec31774a0e9d818dfc6e9242accbbfb4733465f
Hitoshi Iwai <qazsedcdeszaq@gmail.com>
2021-03-15 14:16:45 +0900

    ACL利用

--- atcoder/arc114/b/main.py
+++ atcoder/arc114/b/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import bisect, collections, copy, heapq, itertools, math, operator, string, sys, typing
+from atcoder.dsu import DSU
 input = lambda: sys.stdin.readline().rstrip()
 sys.setrecursionlimit(10 ** 7)
 INF = float('inf')
@@ -12,44 +13,15 @@ def LI_(): return [int(x)-1 for x in input().split()]
 def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
-class UnionFind():
-    def __init__(self, n):
-        self.n = n
-        self.par = list(range(n))
-        self.rank = [0] * n
-
-    def root(self, x):
-        if self.par[x] == x:
-            return x
-        else:
-            r = self.root(self.par[x])
-            self.par[x] = r
-            return r
-
-    def issame(self, x, y):
-        return self.root(x) == self.root(y)
-
-    def merge(self, x, y):
-        x = self.root(x)
-        y = self.root(y)
-        if x == y:
-            return False
-        if self.rank[x] < self.rank[y]:
-            x, y = y, x
-        if self.rank[x] == self.rank[y]:
-            self.rank[x] += 1
-        self.par[y] = x
-        return True
-
 def resolve():
     N = I()
     f = LI_()
 
     # 2 ** 連結成分数 - 1
-    uf = UnionFind(N)
+    dsu = DSU(N)
     for i, e in enumerate(f):
-        uf.merge(i, e)
-    lc_num = len(set(uf.root(i) for i in range(N)))
+        dsu.merge(i, e)
+    lc_num = len(set(dsu.leader(i) for i in range(N)))
 
     ans = pow(2, lc_num, MOD) - 1
     print(ans)
---
+++ atcoder/arc114/b/main_acl.py
@@ -0,0 +1,116 @@
+import types
+
+_atcoder_code = """
+# Python port of AtCoder Library.
+
+__version__ = '0.0.1'
+"""
+
+atcoder = types.ModuleType('atcoder')
+exec(_atcoder_code, atcoder.__dict__)
+
+_atcoder_dsu_code = """
+import typing
+
+
+class DSU:
+    '''
+    Implement (union by size) + (path halving)
+
+    Reference:
+    Zvi Galil and Giuseppe F. Italiano,
+    Data structures and algorithms for disjoint set union problems
+    '''
+
+    def __init__(self, n: int = 0) -> None:
+        self._n = n
+        self.parent_or_size = [-1] * n
+
+    def merge(self, a: int, b: int) -> int:
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        x = self.leader(a)
+        y = self.leader(b)
+
+        if x == y:
+            return x
+
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y = y, x
+
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+
+        return x
+
+    def same(self, a: int, b: int) -> bool:
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        return self.leader(a) == self.leader(b)
+
+    def leader(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        parent = self.parent_or_size[a]
+        while parent >= 0:
+            if self.parent_or_size[parent] < 0:
+                return parent
+            self.parent_or_size[a], a, parent = (
+                self.parent_or_size[parent],
+                self.parent_or_size[parent],
+                self.parent_or_size[self.parent_or_size[parent]]
+            )
+
+        return a
+
+    def size(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        return -self.parent_or_size[self.leader(a)]
+
+    def groups(self) -> typing.List[typing.List[int]]:
+        leader_buf = [self.leader(i) for i in range(self._n)]
+
+        result: typing.List[typing.List[int]] = [[] for _ in range(self._n)]
+        for i in range(self._n):
+            result[leader_buf[i]].append(i)
+
+        return list(filter(lambda r: r, result))
+"""
+
+atcoder.dsu = types.ModuleType('atcoder.dsu')
+exec(_atcoder_dsu_code, atcoder.dsu.__dict__)
+DSU = atcoder.dsu.DSU
+
+#!/usr/bin/env python3
+import bisect, collections, copy, heapq, itertools, math, operator, string, sys, typing
+# from atcoder.dsu import DSU
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10 ** 7)
+INF = float('inf')
+MOD = 998244353
+def I(): return int(input())
+def F(): return float(input())
+def SS(): return input()
+def LI(): return [int(x) for x in input().split()]
+def LI_(): return [int(x)-1 for x in input().split()]
+def LF(): return [float(x) for x in input().split()]
+def LSS(): return input().split()
+
+def resolve():
+    N = I()
+    f = LI_()
+
+    # 2 ** 連結成分数 - 1
+    dsu = DSU(N)
+    for i, e in enumerate(f):
+        dsu.merge(i, e)
+    lc_num = len(set(dsu.leader(i) for i in range(N)))
+
+    ans = pow(2, lc_num, MOD) - 1
+    print(ans)
+
+if __name__ == '__main__':
+    resolve()
