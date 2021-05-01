d10c8662eba47abc337308e6c4ee9be15a1ad614
kanji-a <qazsedcdeszaq@gmail.com>
2021-05-02 00:33:31 +0900

    k

--- atcoder/past202012-open/j/main.py
+++ atcoder/past202012-open/j/main.py
@@ -13,7 +13,31 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    S = SS()
+    X = I() - 1
+
+    # XをSのどれかのアルファベットを指すindexになるまで小さくしていく方針
+
+    # Sの出力の長さ
+    l = [0] * (len(S) + 1)
+    for i in range(len(S)):
+        if S[i] in string.ascii_lowercase:
+            l[i+1] = l[i] + 1
+        else:
+            l[i+1] = l[i] * (int(S[i]) + 1)
+    # print(l)
+
+    idx = len(S) - 1
+    # print(idx)
+    while 0 <= idx:
+        if S[idx].isdigit() and l[idx] < X + 1:
+            X %= l[idx]
+        elif l[idx] == X:
+            break
+        idx -= 1
+        # print(idx, X)
+
+    print(S[X])
 
 if __name__ == '__main__':
     resolve()
--- atcoder/past202012-open/k/main.py
+++ atcoder/past202012-open/k/main.py
@@ -13,7 +13,37 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    S = [SS() for _ in range(4)]
+
+    d = ((0, 0), (1, 0), (0, 1), (-1, 0), (0, -1))
+
+    dp = [INF] * 2 ** 16
+    dp[0] = 0
+
+    for s_c in range(1, 2 ** 16):
+        # どこを狙うのが最適か全探索
+        for cy in range(4):
+            for cx in range(4):
+                # 的を外した数
+                cnt = 0
+                tmp = 0
+                for dy, dx in d:
+                    ny = cy + dy
+                    nx = cx + dx
+                    # 今の状態と次の状態が異なる場合の処理
+                    if 0 <= ny < 4 and 0 <= nx < 4 and s_c >> (ny * 4 + nx) & 1:
+                        s_n = s_c & ~(1 << (ny * 4 + nx))
+                        tmp += (1 + dp[s_n]) / 5
+                    # 今の状態と次の状態が同じ場合の処理
+                    else:
+                        cnt += 1
+                        tmp += 0.2
+                if cnt != 5:
+                    dp[s_c] = min(tmp * 5 / (5 - cnt), dp[s_c])
+
+    s_d = {'.': '0', '#': '1'}
+    ans = dp[int(''.join(''.join(s_d[j] for j in i) for i in S), 2)]
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
