8ed77550d475bd8efc6110a5f13ca5d3e7468259
kanji-a <qazsedcdeszaq@gmail.com>
2021-05-05 19:20:19 +0900

    i

--- atcoder/past202012-open/i/main.py
+++ atcoder/past202012-open/i/main.py
@@ -13,7 +13,33 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N, M, K = LI()
+    H = LI()
+    C = set(LI_())
+    G = collections.defaultdict((list))
+    for _ in range(M):
+        A, B = LI_()
+        if H[A] > H[B]:
+            G[A].append(B)
+        else:
+            G[B].append(A)
+
+    dp = [INF] * N
+    for i, _ in sorted(enumerate(H), key=lambda x: x[1]):
+        if i in C:
+            dp[i] = 0
+            continue
+        tmp = INF
+        for n in G[i]:
+            tmp = min(dp[n], tmp) 
+        dp[i] = tmp + 1
+    # print(dp)
+
+    for i in dp:
+        if i == INF:
+            print(-1)
+        else:
+            print(i)
 
 if __name__ == '__main__':
     resolve()
