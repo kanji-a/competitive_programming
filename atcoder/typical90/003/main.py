6a46cc6b62ccef2185fb1467c2e4f2b9b69ffd8f
kanji-a <qazsedcdeszaq@gmail.com>
2021-05-05 19:20:06 +0900

    3, 6

--- atcoder/typical90/003/main.py
+++ atcoder/typical90/003/main.py
@@ -13,7 +13,39 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N = I()
+    G = collections.defaultdict(list)
+    for _ in range(N - 1):
+        A, B = LI_()
+        G[A].append(B)
+        G[B].append(A)
+
+    d1 = [-1] * N
+    d1[0] = 0
+    que1 = collections.deque()
+    que1.append(0)
+    while que1:
+        c = que1.pop()
+        for n in G[c]:
+            if d1[n] == -1:
+                que1.appendleft(n)
+                d1[n] = d1[c] + 1
+    # print(d1)
+    v = d1.index(max(d1))
+
+    d2 = [-1] * N
+    d2[v] = 0
+    que2 = collections.deque()
+    que2.append(v)
+    while que2:
+        c = que2.pop()
+        for n in G[c]:
+            if d2[n] == -1:
+                que2.appendleft(n)
+                d2[n] = d2[c] + 1
+    # print(d2)
+
+    print(max(d2) + 1)
 
 if __name__ == '__main__':
     resolve()
--- atcoder/typical90/006/main.py
+++ atcoder/typical90/006/main.py
@@ -13,7 +13,37 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N, K = LI()
+    S = SS()
+
+    # first[i][j]: 文字iがj番目以降で初登場するインデックス
+    first = [[-1] * (N + 1) for _ in range(26)]
+    for i in range(26):
+        for j in range(N - 1, -1, -1):
+            if S[j] == chr(ord('a') + i):
+                first[i][j] = j
+            else:
+                first[i][j] = first[i][j + 1]
+    # for i in first:
+    #     print(i)
+
+    ans = []
+    idx = 0
+    for i in range(K):
+        # このような処理を前処理によってスキップ
+        # d = [-1] * 26
+        # for j in range(idx, N - K + 1 + i):
+        #     n = ord(S[j]) - ord('a')
+        #     if d[n] == -1:
+        #         d[n] = j
+        # print(d)
+        for j in range(26):
+            if 0 <= first[j][idx] < N - K + 1 + i:
+                ans.append(chr(ord('a') + j))
+                idx = first[j][idx] + 1
+                break
+
+    print(''.join(ans))
 
 if __name__ == '__main__':
     resolve()
