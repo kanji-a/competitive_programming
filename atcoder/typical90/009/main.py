12b467ff36c4974bffaaf991919e7f780ed2c89c
kanji-a <qazsedcdeszaq@gmail.com>
2021-05-09 22:47:26 +0900

    9

--- atcoder/typical90/009/main.py
+++ atcoder/typical90/009/main.py
@@ -13,7 +13,25 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N = I()
+    XY = [LF()for _ in range(N)]
+
+    ans = 0
+    # まずPjを固定する
+    for j in range(N):
+        # Pjを中心に点Pの偏角を求めてソート
+        a = sorted(math.atan2(XY[i][1] - XY[j][1], XY[i][0] - XY[j][0]) * 180 / math.pi for i in range(N) if i != j)
+        # print(a)
+        for Pi_deg in a:
+            if Pi_deg >= 180:
+                continue
+            idx = bisect.bisect_left(a, Pi_deg + 180)
+            if idx < len(a):
+                ans = max(min(a[idx] - Pi_deg, 360 - a[idx] + Pi_deg), ans)
+            if 0 <= idx - 1:
+                ans = max(min(a[idx-1] - Pi_deg, 360 - a[idx-1] + Pi_deg), ans)
+
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
