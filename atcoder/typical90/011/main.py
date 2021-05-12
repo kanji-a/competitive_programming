7fd9e80e13ed6e09e6cb00e16dcd016d65b1be9c
kanji-a <qazsedcdeszaq@gmail.com>
2021-05-12 23:16:36 +0900

    11

--- atcoder/typical90/011/main.py
+++ atcoder/typical90/011/main.py
@@ -13,7 +13,27 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N = I()
+    DCS = [LI() for _ in range(N)]
+    DCS.sort(key=lambda x: x[0])
+    # print(DCS)
+
+    max_D = DCS[-1][0]
+
+    # dp[i][j]: [:i]番目を選んだときの[:j]日目までの最大報酬
+    dp = [[0] * (max_D + 1) for _ in range(N + 1)]
+    for i in range(N):
+        for j in range(max_D + 1):
+            d, c, s = DCS[i]
+            if c <= j <= d:
+                # 仕事iができる場合
+                dp[i+1][j] = max(dp[i][j-c] + s, dp[i][j])
+            else:
+                dp[i+1][j] = dp[i][j]
+    # for i in dp:
+    #     print(i)
+
+    print(max(dp[-1]))
 
 if __name__ == '__main__':
     resolve()
