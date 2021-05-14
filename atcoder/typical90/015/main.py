11e3563ccae71c7ee3261765c7e67e03f0f7b74b
kanji-a <qazsedcdeszaq@gmail.com>
2021-05-14 23:13:50 +0900

    通ると思ったらTLE

--- atcoder/typical90/015/main.py
+++ atcoder/typical90/015/main.py
@@ -12,8 +12,25 @@ def LI_(): return [int(x)-1 for x in input().split()]
 def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
+def combMod(n, r, p):
+    numer = 1
+    denom = 1
+    for i in range(1, r+1):
+        numer = numer * (n-r+i) % p
+        denom = denom * i % p
+    return numer * pow(denom, p-2, p) % p
+
 def resolve():
-    pass
+    N = I()
+
+    # 差の最小値
+    for k in range(1, N + 1):
+        ans = 0
+        # 取るボールの数
+        for r in range(1, (N - 1) // k + 2):
+            ans += combMod(N - (k - 1) * (r - 1), r, MOD)
+            ans %= MOD
+        print(ans)
 
 if __name__ == '__main__':
     resolve()
