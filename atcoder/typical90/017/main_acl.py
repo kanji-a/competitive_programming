9791d1d0eaa14db3fc3d33521a4f22a05c995a89
kanji-a <qazsedcdeszaq@gmail.com>
2021-05-21 22:01:20 +0900

    WA, TLE

--- atcoder/typical90/017/main_acl.py
+++ atcoder/typical90/017/main_acl.py
@@ -1,6 +1,54 @@
+import types
+
+_atcoder_code = """
+# Python port of AtCoder Library.
+
+__version__ = '0.0.1'
+"""
+
+atcoder = types.ModuleType('atcoder')
+exec(_atcoder_code, atcoder.__dict__)
+
+_atcoder_fenwicktree_code = """
+import typing
+
+
+class FenwickTree:
+    '''Reference: https://en.wikipedia.org/wiki/Fenwick_tree'''
+
+    def __init__(self, n: int = 0) -> None:
+        self._n = n
+        self.data = [0] * n
+
+    def add(self, p: int, x: typing.Any) -> None:
+        assert 0 <= p < self._n
+
+        p += 1
+        while p <= self._n:
+            self.data[p - 1] += x
+            p += p & -p
+
+    def sum(self, left: int, right: int) -> typing.Any:
+        assert 0 <= left <= right <= self._n
+
+        return self._sum(right) - self._sum(left)
+
+    def _sum(self, r: int) -> typing.Any:
+        s = 0
+        while r > 0:
+            s += self.data[r - 1]
+            r -= r & -r
+
+        return s
+"""
+
+atcoder.fenwicktree = types.ModuleType('atcoder.fenwicktree')
+exec(_atcoder_fenwicktree_code, atcoder.fenwicktree.__dict__)
+FenwickTree = atcoder.fenwicktree.FenwickTree
+
 #!/usr/bin/env python3
 import bisect, collections, copy, functools, heapq, itertools, math, operator, string, sys, typing
-from atcoder.fenwicktree import FenwickTree
+# from atcoder.fenwicktree import FenwickTree
 input = lambda: sys.stdin.readline().rstrip()
 sys.setrecursionlimit(10 ** 7)
 INF = float('inf')
