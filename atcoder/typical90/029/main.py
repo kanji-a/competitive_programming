49b4e1606b5275478279346e6104170bf6d14384
kanji-a <qazsedcdeszaq@gmail.com>
2021-05-30 23:17:27 +0900

    29

--- atcoder/typical90/029/main.py
+++ atcoder/typical90/029/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import bisect, collections, copy, functools, heapq, itertools, math, operator, string, sys, typing
+import atcoder.lazysegtree as lazysegtree
 input = lambda: sys.stdin.readline().rstrip()
 sys.setrecursionlimit(10 ** 7)
 INF = float('inf')
@@ -15,6 +16,29 @@ def LSS(): return input().split()
 def resolve():
     W, N = LI()
     
+    def op(a, b):
+        return max(a, b)
+    e = 0
+    def mapping(f, x):
+        if f == -1:
+            return x
+        else:
+            return f
+    def composition(f, g):
+        if f == -1:
+            return g
+        else:
+            return f
+    id_ = -1
+    v = W
+    lst = lazysegtree.LazySegTree(op, e, mapping, composition, id_, v)
+
+    for _ in range(N):
+        L, R = LI_()
+        R += 1
+        ans = lst.prod(L, R) + 1
+        print(ans)
+        lst.apply(L, R, ans)
 
 if __name__ == '__main__':
     resolve()
