ee9083b30a97b7c9125ff5004248086bcc0d69bb
kanji-a <qazsedcdeszaq@gmail.com>
2021-05-31 22:02:43 +0900

    32

--- atcoder/typical90/032/main.py
+++ atcoder/typical90/032/main.py
@@ -13,7 +13,32 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N = I()
+    A = [LI() for _ in range(N)]
+    M = I()
+    XY_ = [LI_() for _ in range(M)]
+    XY = set()
+    for i, j in XY_:
+        XY.add((i, j))
+        XY.add((j, i))
+
+    ans = INF
+    for i in itertools.permutations(range(N)):
+        is_ok = True
+        for j in range(N - 1):
+            if (i[j], i[j+1]) in XY:
+                is_ok = False
+                break
+        if is_ok:
+            t = 0
+            for j, e in enumerate(i):
+                t += A[e][j]
+            ans = min(t, ans)
+
+    if ans == INF:
+        print(-1)
+    else:
+        print(ans)
 
 if __name__ == '__main__':
     resolve()
