bd4eb00db92a24af90e382e09f9d5a2158c45504
kanji-a <qazsedcdeszaq@gmail.com>
2021-06-06 01:36:13 +0900

    d

--- atcoder/abc203/d/main.py
+++ atcoder/abc203/d/main.py
@@ -16,20 +16,38 @@ def resolve():
     N, K = LI()
     A = [LI() for _ in range(N)]
 
-    # 決め打ち二分探索 → 中央値作れるか2分できなさそう
-    # K*Kの区画を全探索 → 間に合わない
-    # 中央値が低いを言い換える
-    ans = INF
-    for i in range(N - K + 1):
-        for j in range(N - K + 1):
-            tmp = []
-            for k in range(K):
-                for l in range(K):
-                    tmp.append(A[i+k][j+l])
-            tmp.sort(reverse=True)
-            # print(tmp)
-            ans = min(tmp[K**2//2], ans)
-    print(ans)
+    # 決め打ち二分探索
+
+    # x以下の中央値を持つ区画はあるか
+    # →xがK^2//2+1番目以下になる区画はあるか
+    # →x以上の数がK^2//2+1個以下ある区画はあるか
+    def is_ok(x):
+        acm = [[0] * (N + 1) for _ in range(N + 1)]
+        for i in range(N):
+            for j in range(N):
+                acm[i+1][j+1] = acm[i+1][j] + (A[i][j] > x)
+        for i in range(N):
+            for j in range(N):
+                acm[i+1][j+1] += acm[i][j+1]
+        # for i in acm:
+        #     print(i)
+        res = False
+        for i in range(N - K + 1):
+            for j in range(N - K + 1):
+                num = acm[i+K][j+K] - acm[i+K][j] - acm[i][j+K] + acm[i][j]
+                if num <= K ** 2 // 2:
+                    res = True
+        return res
+
+    ng = -1
+    ok = 10 ** 9
+    while abs(ok - ng) > 1:
+        m = (ng + ok) // 2
+        if is_ok(m):
+            ok = m
+        else:
+            ng = m
+    print(ok)
 
 if __name__ == '__main__':
     resolve()
