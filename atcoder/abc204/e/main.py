59feffda18582e54dadbf8af93fdb8ba28ab5e61
kanji-a <qazsedcdeszaq@gmail.com>
2021-06-07 12:24:22 +0000

    e

--- atcoder/abc204/e/main.py
+++ atcoder/abc204/e/main.py
@@ -12,22 +12,6 @@ def LI_(): return [int(x)-1 for x in input().split()]
 def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
-def dijkstra(G, N, s):
-    que = []
-    d = [INF] * N
-    d[s] = 0
-    heapq.heappush(que, (d[s], s))
-
-    while que:
-        p = heapq.heappop(que)
-        v = p[1]
-        if d[v] < p[0]: continue
-        for e in G[v]:
-            if d[e[0]] > d[v] + e[1]:
-                d[e[0]] = d[v] + e[1]
-                heapq.heappush(que, (d[e[0]], e[0]))
-    return d
-
 def resolve():
     N, M = LI()
     G = collections.defaultdict(list)
@@ -36,6 +20,7 @@ def resolve():
         A -= 1
         B -= 1
         G[A].append((B, C, D))
+        G[B].append((A, C, D))
 
     que = []
     d = [INF] * N
@@ -49,18 +34,12 @@ def resolve():
         if d[v] < p[0]: continue
         for e in G[v]:
             # 移動時間が最小になる時刻
-            t0 = max(int(D ** 0.5) - 1, 0)
-            t1 = int(D ** 0.5)
-            next_time0 = t0 + e[1] + e[2] // (t0 + 1)
-            next_time1 = t1 + e[1] + e[2] // (t1 + 1)
-            t = -1
-            if next_time0 < next_time1:
-                t = t0
-            else:
-                t = t1
-            t = max(d[v], t)
-            if d[e[0]] > t + e[1] + e[2] // (t + 1):
-                d[e[0]] = t + e[1] + e[2] // (t + 1)
+            sqrtD = int(e[2] ** 0.5)
+            tmp = d[v] + e[1] + e[2] // (d[v] + 1)
+            for t in range(max(sqrtD - 5, d[v]), sqrtD + 6):
+                tmp = min(t + e[1] + e[2] // (t + 1), tmp)
+            if d[e[0]] > tmp:
+                d[e[0]] = tmp
                 heapq.heappush(que, (d[e[0]], e[0]))
     # print(d)
 
