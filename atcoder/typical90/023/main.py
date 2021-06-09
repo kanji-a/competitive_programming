6446bbad91b8d48377ed988edb1bb5e2477525e8
kanji-a <qazsedcdeszaq@gmail.com>
2021-06-09 23:18:13 +0900

    bitDPでやろうとしている途中

--- atcoder/typical90/023/main.py
+++ atcoder/typical90/023/main.py
@@ -13,7 +13,39 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    H, W = LI()
+    C = [SS() for _ in range(W)]
+
+    # bitDP 今の行の置き方に影響を与えるのは1行前の状態のみ
+    dp = [[0] * 2 ** W for _ in range(H)]
+    for i in range(2 ** W):
+        # #に置くパターンは弾く
+        is_ok = True
+        for j in range(W):
+            if i >> j and C[0][j] == '#':
+                is_ok = False
+                break
+        # そのマスに置けるか
+        tmp = [True] * W
+        for j in range(W):
+            if i >> j:
+                if 0 <= j - 1:
+                    tmp[j-1] = False
+                if j + 1 < W:
+                    tmp[j+1] = False
+        is_ok = True
+        for j in range(W):
+            if i >> j and not tmp[j]:
+                is_ok = False
+                break
+        if is_ok:
+            dp[0][i] = 1
+    print(dp[0])
+
+
+    # for i in range(H - 1):
+    #     for j in range(2 ** W):
+    #         for k in range(2 ** W):
 
 if __name__ == '__main__':
     resolve()
