5116d86edefe683f21fa836df07dd6d8e592c161
kanji-a <qazsedcdeszaq@gmail.com>
2021-06-14 01:14:50 +0900

    39

--- atcoder/typical90/039/main.py
+++ atcoder/typical90/039/main.py
@@ -13,7 +13,33 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N = I()
+    G = collections.defaultdict(list)
+    for _ in range(N - 1):
+        a, b = LI_()
+        G[a].append(b)
+        G[b].append(a)
+
+    # 辺ごとの登場回数を集計する
+    desc = [0] * N
+
+    def dfs(c, p):
+        sum_desc = 1
+        child_count = 0
+        for n in G[c]:
+            if n != p:
+                child_count += 1
+                dfs(n, c)
+                sum_desc += desc[n]
+        desc[c] = sum_desc
+
+    dfs(0, -1)
+    # print(desc)
+
+    # 各辺の登場回数は、深い方の頂点とその子孫と、それ以外の頂点の積
+    # 木なので各辺を頂点に対応付けられる
+    ans = sum(desc[i] * (N - desc[i]) for i in range(N))
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
