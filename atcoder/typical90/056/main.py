e860dd9b36b56eca24dabb534996051346164070
kanji-a <qazsedcdeszaq@gmail.com>
2021-06-19 01:04:14 +0900

    AC

--- atcoder/typical90/056/main.py
+++ atcoder/typical90/056/main.py
@@ -13,7 +13,37 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N, S = LI()
+    AB = [LI() for _ in range(N)]
+
+    # dp[i]: i円になるように購入できるか
+    dp = [[False] * (S + 1) for _ in range(N + 1)]
+    dp[0][0] = True
+
+    for i in range(N):
+        A, B = AB[i]
+        for j in range(S, 0, -1):
+            if 0 <= j - A and dp[i][j-A]:
+                dp[i+1][j] = True
+            if 0 <= j - B and dp[i][j-B]:
+                dp[i+1][j] = True
+    # for i in dp:
+    #     print(i)
+
+    if dp[-1][-1]:
+        ans_r = []
+        s = S
+        for i in range(N - 1, -1, -1):
+            A, B = AB[i]
+            if 0 <= s - A and dp[i][s-A]:
+                ans_r.append('A')
+                s -= A
+            elif 0 <= s - B and dp[i][s-B]:
+                ans_r.append('B')
+                s -= B
+        print(''.join(ans_r[::-1]))
+    else:
+        print('Impossible')
 
 if __name__ == '__main__':
     resolve()
