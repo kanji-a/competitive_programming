3f1dcdbfce62967aa86e4e96b3f2621d17a087d9
kanji-a <qazsedcdeszaq@gmail.com>
2021-06-24 00:48:57 +0900

    TLE

--- atcoder/typical90/055/main.py
+++ atcoder/typical90/055/main.py
@@ -13,7 +13,24 @@ def LF(): return [float(x) for x in input().split()]
 def LSS(): return input().split()
 
 def resolve():
-    pass
+    N, P, Q = LI()
+    A = LI()
+
+    ans = 0
+    for i in range(N - 4):
+        tmp0 = A[i]
+        for j in range(i + 1, N - 3):
+            tmp1 = tmp0 * A[j]
+            for k in range(j + 1, N - 2):
+                tmp2 = tmp1 * A[k]
+                for l in range(k + 1, N - 1):
+                    tmp3 = tmp2 * A[l]
+                    for m in range(l + 1, N):
+                        tmp4 = tmp3 * A[m]
+                        if tmp4 % P == Q:
+                            ans += 1
+
+    print(ans)
 
 if __name__ == '__main__':
     resolve()
