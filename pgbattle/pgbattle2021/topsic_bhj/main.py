31f650309d5ede7a40c74411dd62b83b19c3171e
kanji-a <qazsedcdeszaq@gmail.com>
2021-10-24 01:10:51 +0900

    AC

--- pgbattle/pgbattle2021/topsic_bhj/main.py
+++ pgbattle/pgbattle2021/topsic_bhj/main.py
@@ -16,45 +16,32 @@ def resolve():
     N, S = LI()
     d = LI()
 
-    # 小さい桁数の方から限界まで大きく使ってSを減らしていく
-
     cnt = collections.Counter(d)
-    # d_idx[i]: 値がiになっている箇所のインデックス
-    d_idx = [[] for _ in range(10)]
-    for i in range(N):
-        d_idx[d[i]].append(i)
+    # aで実現可能なSの範囲
+    S_max = sum((10 ** k - 1) * v for k, v in cnt.items())
+    S_min = sum(10 ** (k - 1) * v for k, v in cnt.items())
 
-    # ans_pre[i]: i桁の数の合計
-    ans_pre = [0] * 10
-    is_ok = True
-    for i in range(1, 10):
-        # その桁数*個数で表せる最大の数
-        lim_max = (10 ** i - 1) * cnt[i]
-        lim_min = (10 ** (i - 1)) * cnt[i]
-        if lim_min <= S <= lim_max:
-            ans_pre[i] = S
-            S = 0
-            break
-        elif S > lim_max:
-            ans_pre[i] = lim_max
-            S -= lim_max
-        else:
-            is_ok = False
-
-    if is_ok and S == 0:
-        # print(ans_pre)
-        a = [0] * N
-        for i in range(1, 10):
-            n = 10 ** i - 1
-            q = ans_pre[i] // n
-            r = ans_pre[i] % n
-            for j in range(q):
-                a[d_idx[i][j]] = n
-            if q < len(d_idx[i]):
-                a[d_idx[i][-1]] = r
-        print(* a)
-    else:
+    # 範囲外の場合は作れない
+    if S < S_min or S_max < S:
         print(-1)
+        sys.exit()
+
+    # 範囲に入っていれば必ず作れる
+
+    # 各a_iには事前に最小値を設定しておき、前から順に和がSになるよう数字を増やしていく
+    a = [10 ** (i - 1) for i in d]
+    S -= S_min
+    for i in range(N):
+        m = 10 ** d[i] - 1
+        # print(S, m, i)
+        if S >= m - a[i]:
+            S -= m - a[i]
+            a[i] = m
+        else:
+            a[i] += S
+            break
+
+    print(*a)
 
 if __name__ == '__main__':
     resolve()
