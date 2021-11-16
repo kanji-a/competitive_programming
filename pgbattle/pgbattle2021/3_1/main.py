d5c8bc054997d8d7d97aa367ac6f5d9a2419ac1f
kanji-a <qazsedcdeszaq@gmail.com>
2021-11-16 22:12:28 +0900

    自分で書いたコードに変更

--- pgbattle/pgbattle2021/3_1/main.py
+++ pgbattle/pgbattle2021/3_1/main.py
@@ -15,26 +15,7 @@ def LSS(): return input().split()
 def resolve():
     N = I()
 
-    # 引用元 https://www.geeksforgeeks.org/count-digits-factorial-set-1/
-    def findDigits(n):
-        
-        # factorial exists only for n>=0
-        if (n < 0):
-            return 0;
-    
-        # base case
-        if (n <= 1):
-            return 1;
-    
-        # else iterate through n and
-        # calculate the value
-        digits = 0;
-        for i in range(2, n + 1):
-            digits += math.log10(i);
-    
-        return math.floor(digits) + 1;
-
-    ans = findDigits(N)
+    ans = int(sum(math.log10(i) for i in range(1, N + 1)) + 1)
     print(ans)
 
 if __name__ == '__main__':
