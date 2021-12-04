d30743576cfa6d5f1e48767f56312ed0addb9470
kanji-a <qazsedcdeszaq@gmail.com>
2021-12-05 00:52:12 +0900

    トーナメント表に順位を割り当てるところ以外を実装

--- pgbattle/pgbattle2021/2_3/main.py
+++ pgbattle/pgbattle2021/2_3/main.py
@@ -16,10 +16,28 @@ def resolve():
     N = I()
     a = LI()
 
-    c = collections.Counter(a)
+    cnt = collections.Counter(a)
 
     # OKな必要十分条件: ベスト1が1人、ベスト2^xが2^(x-1)人であること。
     # aは2の累乗という条件があるので、ベスト1が1人、ベストxがx//2人であること、でよい。
+    cnt_ok = {2 ** (i + 1): 2 ** i for i in range(N)}
+    cnt_ok[1] = 1
+    print(cnt, cnt_ok)
+    if cnt != cnt_ok:
+        print(-1)
+        sys.exit()
+
+    # 順位の並びを決めてから、その順位になる人の番号を割り振る
+    # 8人だとしたら、順位を1 8 4 8 2 8 4 8のような並び方にしたい
+    rank = [0] * 2 ** N
+
+    # 順位: 人の番号一覧 という辞書を作り、答えを作る
+    dict_rank_num = {k: [i[0] for i in g] for k, g in itertools.groupby(sorted(enumerate(a), key=lambda x: x[1]), key=lambda x: x[1])}
+    print(dict_rank_num)
+    ans = [0] * 2 ** N
+    for i in range(2 ** N):
+        ans[i] = dict_rank_num[rank[i]].pop()
+    print(*ans)
 
 if __name__ == '__main__':
     resolve()
